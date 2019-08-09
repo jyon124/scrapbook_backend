1a5ba4ceb528457104093af1488c8f5c5fc54f05
Jooyon Won <jyon1204@gmail.com>
2019-08-08 20:57:33 -0500

    Authentication Setup - backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
   resources :news
   resources :highlights
   resources :notes
