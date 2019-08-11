2be627e943f4d38b915ffd488ec7fa2a96a5ca81
Jooyon Won <jyon1204@gmail.com>
2019-08-11 14:39:57 -0500

    Primary Key News Issue Fixed

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,8 +1,13 @@
 class NewsController < ApplicationController
-  # skip_before_action :authorized, only: [:index]
+  skip_before_action :authorized, only: [:index, :show]
 
     def index
       @news = News.all
+      render json: @news
+    end
+
+    def show
+      @news = News.find_by(id: params[:id])
       render json: @news
     end
 
