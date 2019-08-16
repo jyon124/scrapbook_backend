b3e49db65a3e1f0ab7f5e6317960cea8b2111b56
Jooyon Won <jyon1204@gmail.com>
2019-08-15 23:16:26 -0500

    Highlight Controller

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -6,13 +6,14 @@ class HighlightsController < ApplicationController
     end
     
     def create
-    highlight = Highlight.create(scrapbooknews_id: params['scrapbooknews_id'], sentence: params['sentence'], color: params['color'])
-    render json: highlight
+      highlight = Highlight.create(scrapbooknews_id: params['scrapbooknews_id'], sentence: params['sentence'], color: params['color'])
+      render json: highlight
     end
 
     def destroy
-    highlight = Highlight.find(params['id'])
-    highlight.destroy
+      highlight = Highlight.find(params['id'])
+      highlight.destroy
+      render json: {message:"successfuly deleted"}, status: :no_content
     end
 
 end
