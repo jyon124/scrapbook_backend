87d9a280b8e28ce312494fa73d0e2ce89192f978
Jooyon Won <jyon1204@gmail.com>
2019-09-01 16:28:56 -0400

    News Index & Show skip before action

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,5 @@
 class NewsController < ApplicationController
-  # skip_before_action :authorized, only: [:index, :show]
+  skip_before_action :authorized, only: [:index, :show, :update]
 
     def index
       @news = News.all
