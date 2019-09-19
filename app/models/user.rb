977339fc393a8148dbe28b0c656c18534a95c427
Jooyon Won <jyon1204@gmail.com>
2019-09-19 18:21:12 -0400

    Validation Modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, uniqueness: { case_sensitive: false }
+
     has_one :scrapbook
 
+    validates :username, uniqueness: { case_sensitive: false }
+    validates :username, length: {minimum: 2}, allow_blank: false
+    validates :name, length: {minimum: 2}, allow_blank: false
+    validates :password, length: { minimum: 3 }, allow_nil: false
+    
 end
